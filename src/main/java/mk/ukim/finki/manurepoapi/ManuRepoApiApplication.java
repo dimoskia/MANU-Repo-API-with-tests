824606dddd2ad77f4a5a8d65545f0b311ea6afba
Aleksandar Dimoski <dimoskialeksandar11@gmail.com>
2021-01-16 15:29:31 +0100

    Initialize empty Spring Boot project

---
+++ src/main/java/mk/ukim/finki/manurepoapi/ManuRepoApiApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.manurepoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuRepoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManuRepoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/manurepoapi/ManuRepoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.manurepoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuRepoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
