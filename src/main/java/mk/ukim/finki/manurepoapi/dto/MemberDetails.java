e38eb5888e62be43afb0ad8cac61a22fd543d3d3
Aleksandar Dimoski <dimoskialeksandar11@gmail.com>
2021-01-23 20:11:16 +0100

    Add missing properties to MemberDetails DTO

--- src/main/java/mk/ukim/finki/manurepoapi/dto/MemberDetails.java
+++ src/main/java/mk/ukim/finki/manurepoapi/dto/MemberDetails.java
@@ -4,10 +4,24 @@ import lombok.Getter;
 import lombok.Setter;
 import mk.ukim.finki.manurepoapi.enums.AcademicDegree;
 import mk.ukim.finki.manurepoapi.enums.AcademicRank;
+import mk.ukim.finki.manurepoapi.enums.Department;
+import mk.ukim.finki.manurepoapi.enums.MemberType;
 
 @Getter
 @Setter
 public class MemberDetails {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Department department;
+
+    private MemberType memberType;
+
+    private String imageUrl;
 
     private String email;
 
@@ -27,6 +41,14 @@ public class MemberDetails {
 
     public String getAcademicRank() {
         return academicRank.getFullRank();
+    }
+
+    public String getMemberType() {
+        return memberType.getFullType();
+    }
+
+    public String getDepartment() {
+        return department.getFullDepartment();
     }
 
 }
