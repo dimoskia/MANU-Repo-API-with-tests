5df2378fd3391985778c806b9993376a55eaf1fc
Aleksandar Dimoski <dimoskialeksandar11@gmail.com>
2021-01-31 17:40:23 +0100

    Enable CORS

--- src/main/java/mk/ukim/finki/manurepoapi/ManuRepoApiApplication.java
+++ src/main/java/mk/ukim/finki/manurepoapi/ManuRepoApiApplication.java
@@ -2,7 +2,10 @@ package mk.ukim.finki.manurepoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,15 @@ public class ManuRepoApiApplication {
         SpringApplication.run(ManuRepoApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
