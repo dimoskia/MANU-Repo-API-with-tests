1b3fe3d5b58fb0be14896fbecfcdf239ed72772a
Aleksandar Dimoski <dimoskialeksandar11@gmail.com>
2021-08-16 23:14:12 +0200

    Change Spring Security config

--- src/main/java/mk/ukim/finki/manurepoapi/security/config/SecurityConfiguration.java
+++ src/main/java/mk/ukim/finki/manurepoapi/security/config/SecurityConfiguration.java
@@ -29,16 +29,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/accounts", "/accounts/confirmRegistration", "/accounts/emailAvailable").permitAll()
                 .antMatchers("/accounts/**").authenticated()
                 .antMatchers(HttpMethod.POST, "/files").authenticated()
                 .antMatchers("/records/manage").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .anyRequest().permitAll();
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
